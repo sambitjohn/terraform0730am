6870fd82f37093bf005166c6b27bfd7a86009480
veera <veerababu.narni@gmail.com>
2025-01-03 08:49:15 +0530

    modified-day-3

--- day-3-statefile/main.tf
+++ day-3-statefile/main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "dev" {
     ami = "ami-0fd05997b4dff7aac"
     instance_type = "t2.micro"
     key_name = "aws_key"
-    availability_zone = "ap-south-1b"
+    availability_zone = "ap-south-1a"
    
 }
