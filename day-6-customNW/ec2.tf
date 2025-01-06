697353cdd37201e73fec0b3e41600bb90b247d02
veera <veerababu.narni@gmail.com>
2025-01-06 08:23:25 +0530

    day-6

---
+++ day-6-customNW/ec2.tf
@@ -0,0 +1,16 @@
+#Create public Servers 
+
+resource "aws_instance" "dev" {
+    ami = "ami-0fd05997b4dff7aac"
+    instance_type = "t2.micro"
+    key_name = "aws_key"
+    subnet_id = aws_subnet.public.id
+    vpc_security_group_ids = [ aws_security_group.allow_tls.id ]
+    associate_public_ip_address = true
+    tags = {
+      Name = "dev_ec2"
+    }
+  
+}
+
+# Create private server 
